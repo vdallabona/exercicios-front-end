5702e9fef41e556b85e39d21b0280cd85c3dcb5c
vdallabona <vinicius.dallabona@gmail.com>
2024-08-22 19:48:28 -0300

    ajustando front-end para o JS

--- exercicio 4/scripts/form.js
+++ exercicio 4/scripts/form.js
@@ -1,12 +1,9 @@
 document.getElementById('form').addEventListener('submit', (event) => {
+
     const email = document.getElementById('emailLogin').value;
-    const senha = document.getElementById('passwordLogin').value;
     if (!email) {
-        alert('Por favor, preencha o campo email');
+        alert('Por favor, digite algo para adicionar na lista');
         event.preventDefault();
     }
-    if (!senha) {
-        alert('Por favor, preencha o campo senha');
-        event.preventDefault();
-    }
+
 });
